050bc4271ccf24bb1166b35dbadd63dc291c1c97
Dereck <derecknova23@gmail.com>
2024-04-15 12:33:38 -0500

    Diego solo agregó una variable

--- src/main/java/pe/edu/utp/csvsorterandsearcher/CSVReader/CSVReader.java
+++ src/main/java/pe/edu/utp/csvsorterandsearcher/CSVReader/CSVReader.java
@@ -8,12 +8,17 @@ public class CSVReader {
     FileInputStream file;
     BufferedReader reader;
 
+    boolean use_file_headers;
+
     CSVReader(String path) {
         try {
             this.file = new FileInputStream(path);
             this.reader = new BufferedReader(reader);
+            use_file_headers = false;
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
     }
+
+
 }
