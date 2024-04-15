12423d71a54e1b2c3a46bb2146ec4d4d45bf4f78
Dereck <derecknova23@gmail.com>
2024-04-15 09:28:20 -0500

    se agregó DataTypeDetector

---
+++ src/main/java/pe/edu/utp/csvsorterandsearcher/Utilities/DataTypeDetector.java
@@ -0,0 +1,44 @@
+package pe.edu.utp.csvsorterandsearcher.Utilities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class DataTypeDetector {
+
+    private static boolean isDouble(String value){
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch (NumberFormatException e){
+            return false;
+        }
+    }
+
+    private static boolean isDate(String value){
+        try{
+            LocalDate.parse(value);
+            return true;
+        }catch (DateTimeParseException e){
+            return false;
+        }
+    }
+
+    private static boolean isBoolean(String value){
+        return value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false");
+    }
+
+    public static FieldType DetectarTipoDato(String value){
+        if(DataTypeDetector.isDouble(value)){
+            return FieldType.Numeric;
+        } else if (DataTypeDetector.isDate(value)) {
+            return FieldType.Date;
+
+        } else if (DataTypeDetector.isBoolean(value)){
+            return FieldType.Boolean;
+        }
+        else{
+            return FieldType.String;
+        }
+    }
+
+}
